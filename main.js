c3b598499c3a810cff69a216d7b598f4a5e53907
iskr889 <68342806+iskr889@users.noreply.github.com>
2023-03-19 17:13:38 -0300

    Update main.js

--- main.js
+++ main.js
@@ -5,3 +5,28 @@ function mostrar_menu(){
     document.getElementById("move-content").classList.toggle('move-container-all');
     document.getElementById("show-menu").classList.toggle('show-lateral');
 }
+
+/*formulario*/
+
+function validar(event) {
+	event.preventDefault();
+	var nombre = document.getElementById("name").value;
+    var correo = document.getElementById("mail").value;
+    var mensaje = document.getElementById("message").value;
+	
+    if (nombre == "" || correo == "" || mensaje == "") {
+      alert("Por favor complete todos los campos del formulario.");
+      return false;
+    }
+
+    if (!/\S+@\S+\.\S+/.test(correo)) {
+      alert("Por favor ingrese un correo electrónico válido.");
+      return false;
+    }
+
+    document.contact.submit();
+}
+
+const form = document.querySelector("form[name='contact']");
+form.addEventListener("submit", validar);
+
